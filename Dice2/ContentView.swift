1321f50e79902e741423851b284b0ce7cc5d78bf
Ma Xueyuan <machinemxy@me.com>
2020-06-18 22:10:41 +0900

    initial commit

---
+++ Dice2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice2
+//
+//  Created by Ma Xueyuan on 2020/06/18.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
